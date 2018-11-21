6f73b1d54684ee2f999e9ff713c0e3c6dca567b1
Tim Creasman <tjcreasm@ncsu.edu>
2018-11-20 23:31:27 -0500

    Updated landing page and added nav bar

--- src/Index.vue
+++ src/Index.vue
@@ -1,8 +1,15 @@
 <template>
   <div class='container'>
-    <div class='header'></div>
+    <div class='header'>
+      <a class='nav-button' href='about.html'>About</a>
+      <a class='nav-button' href='projects.html'>Projects</a>
+      <a class='nav-button' href='faith.html'>Faith</a>
+      <a class='nav-button' href='shoutout.html'>Shoutout</a>
+      <a class='nav-button' href='contact.html'>Contact</a>
+    </div>
     <div class='welcome'>
-      Welcome
+      <div class='name-tim'>Tim</div>
+      <div class='name-creasman'>Creasman</div>
     </div>
     <div class='wip'>check back later for updates...</div>
     <div class ='footer'></div>
